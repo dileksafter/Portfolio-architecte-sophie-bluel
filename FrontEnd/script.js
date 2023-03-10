fc4e6518e3c15c53e437cac1b22437cf3f580cb6
Dilek SAFTER <d.safter@yahoo.fr>
2023-03-10 16:00:09 +0100

    step 1.2 done

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,7 +2,8 @@
 async function getProjects() {
     const projects = await fetch("http://localhost:5678/api/works")
         .then(response => response.json())
-    return projects
+        .then(projects => Array.from(new Set(projects))) //verification et elimination des eventuels duplicats de travaux 
+        return projects
 }
 
 //ajout des travaux recuperés dans la gallerie
@@ -11,15 +12,15 @@ function addElements(projects) {
     const gallery = document.querySelector("#portfolio > .gallery");
 
     //creating a for cycle to target each index of the array
-  for (let i = 0; i < projects.length; i++){ 
-    
-    //creation d'elements dans la gallerie en html
+    for (let i = 0; i < projects.length; i++) {
+
+        //creation d'elements dans la gallerie en html
         const newFigure = document.createElement("figure");
 
         const newImage = document.createElement("img");
 
         const newCaption = document.createElement("figcaption");
-   
+
         //on accede a l'indice i des travaux recuperés pour configurer les parametres de l'image
         newImage.setAttribute("src", projects[i].imageUrl)
         newImage.setAttribute("alt", projects[i].title)
@@ -30,7 +31,7 @@ function addElements(projects) {
         //on rattache la figure a la gallerie
         gallery.appendChild(newFigure)
 
-          //on rattache la image et le titre a la figure
+        //on rattache la image et le titre a la figure
         newFigure.appendChild(newImage)
         newFigure.appendChild(newCaption)
 
@@ -42,7 +43,7 @@ async function filterElements() {
 
     const projects = await getProjects()
 
-    //afficher a l'ecran tous les elements du swagger 
+    //afficher a l'ecran tous les elements du swagger par defaut 
     addElements(projects)
 
     console.log(projects)
@@ -56,44 +57,46 @@ async function filterElements() {
 
     const buttonHôtelsetrestaurants = document.querySelector(".button-hôtelsetrestaurants");
 
-
-    
+    //cible de la gallerie pour l'etape suivante
+    const gallery = document.querySelector("#portfolio > .gallery")
 
     buttonTous.addEventListener("click", function () {
+        gallery.innerHTML = "" //on vide le contenu de la gallerie au click grace a innerHTML
         console.log(projects)
-        addElements(projects)
+        addElements(projects) //on appelle la fonction qui ajoute les elements a lecran 
     })
-
-    
 
     buttonObjets.addEventListener("click", function () {
+        gallery.innerHTML = "" //on vide le contenu de la gallerie au click grace a innerHTML
+
+        //on creait la constante filter qui retourne tous les elements de projects categoryId = 1 qui correspond a Objets 
         const filtreObjet = projects.filter(function (project) {
             return project.categoryId == 1;
-
         });
         console.log(filtreObjet)
-        addElements(filtreObjet)
-
+        addElements(filtreObjet) //on appelle la fonction qui ajoute uniquement les elements filtrés a lecran 
     })
 
-    
     buttonAppartements.addEventListener("click", function () {
+        gallery.innerHTML = "" //on vide le contenu de la gallerie au click grace a innerHTML
+
+        //on creait la constante filter qui retourne tous les elements de projects categoryId = 2 qui correspond a Appartements 
         const filtreAppartements = projects.filter(function (project) {
             return project.categoryId == 2;
-
         });
         console.log(filtreAppartements)
-        addElements(filtreAppartements)
+        addElements(filtreAppartements) //on appelle la fonction qui ajoute uniquement les elements filtrés a lecran 
     })
 
-    
     buttonHôtelsetrestaurants.addEventListener("click", function () {
+        gallery.innerHTML = "" //on vide le contenu de la gallerie au click grace a innerHTML
+
+        //on creait la constante filter qui retourne tous les elements de projects categoryId = 3 qui correspond a Hôtelsetrestaurants 
         const filtreHôtelsetrestaurants = projects.filter(function (project) {
             return project.categoryId == 3;
-
         });
         console.log(filtreHôtelsetrestaurants)
-        addElements(filtreHôtelsetrestaurant)
+        addElements(filtreHôtelsetrestaurants) //on appelle la fonction qui ajoute uniquement les elements filtrés a lecran 
     })
 
 }
