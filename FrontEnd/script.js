2fb0ea1be96cc8bafe2efb53198b13c2f75d8eca
Dilek SAFTER <d.safter@yahoo.fr>
2023-03-10 14:56:35 +0100

    step 1.2 wip

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,52 +1,104 @@
-/*recuperation des travaux dans le swagger*/
+//recuperation des travaux dans le swagger
 async function getProjects() {
     const projects = await fetch("http://localhost:5678/api/works")
         .then(response => response.json())
-        .then(dataProjects => dataProjects /*est ce necessaire de faire un then puis return, reciving end returning it*/
-        )
     return projects
 }
 
-/*ajout des travaux recuperés dans la gallerie*/
-async function addElements() {
-    const projects = await getProjects()
+//ajout des travaux recuperés dans la gallerie
+function addElements(projects) {
 
     const gallery = document.querySelector("#portfolio > .gallery");
 
-    for (const project of projects) {
-
+    //creating a for cycle to target each index of the array
+  for (let i = 0; i < projects.length; i++){ 
+    
+    //creation d'elements dans la gallerie en html
         const newFigure = document.createElement("figure");
 
         const newImage = document.createElement("img");
 
         const newCaption = document.createElement("figcaption");
+   
+        //on accede a l'indice i des travaux recuperés pour configurer les parametres de l'image
+        newImage.setAttribute("src", projects[i].imageUrl)
+        newImage.setAttribute("alt", projects[i].title)
 
-        newImage.setAttribute("src", project.imageUrl)
-        newImage.setAttribute("alt", project.title)
+        //on rattache titre au contenu texte
+        newCaption.append(projects[i].title)
 
-        gallery.append(newFigure)
+        //on rattache la figure a la gallerie
+        gallery.appendChild(newFigure)
 
-        newFigure.append(newImage, newCaption)
+          //on rattache la image et le titre a la figure
+        newFigure.appendChild(newImage)
+        newFigure.appendChild(newCaption)
 
-        newCaption.append(project.title)
     }
+}
+
+/*filtre des elemenst de la gallerie*/
+async function filterElements() {
+
+    const projects = await getProjects()
+
+    //afficher a l'ecran tous les elements du swagger 
+    addElements(projects)
+
+    console.log(projects)
+
+    //cible les boutons filtres crees en Html 
+    const buttonTous = document.querySelector(".button-tous");
 
     const buttonObjets = document.querySelector(".button-objets");
-    console.log(buttonObjets)
+
+    const buttonAppartements = document.querySelector(".button-appartements");
+
+    const buttonHôtelsetrestaurants = document.querySelector(".button-hôtelsetrestaurants");
+
+
+    
+
+    buttonTous.addEventListener("click", function () {
+        console.log(projects)
+        addElements(projects)
+    })
+
+    
+
     buttonObjets.addEventListener("click", function () {
         const filtreObjet = projects.filter(function (project) {
             return project.categoryId == 1;
 
         });
         console.log(filtreObjet)
+        addElements(filtreObjet)
+
+    })
+
+    
+    buttonAppartements.addEventListener("click", function () {
+        const filtreAppartements = projects.filter(function (project) {
+            return project.categoryId == 2;
+
+        });
+        console.log(filtreAppartements)
+        addElements(filtreAppartements)
+    })
+
+    
+    buttonHôtelsetrestaurants.addEventListener("click", function () {
+        const filtreHôtelsetrestaurants = projects.filter(function (project) {
+            return project.categoryId == 3;
+
+        });
+        console.log(filtreHôtelsetrestaurants)
+        addElements(filtreHôtelsetrestaurant)
     })
 
 }
 
-addElements()
 
-
-const projects = getProjects()
-
+filterElements()
 
 
