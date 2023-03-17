69b94f3bd6abebf0d792116c50d76ef50590f5c9
Dilek SAFTER <d.safter@yahoo.fr>
2023-03-17 18:02:46 +0100

    step 2.2 wip

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,32 +1,39 @@
+//on declare la fonction qui recupere les donnees entrees par l'utilisateur sur l'interface du site
+function getValues() {
 
-async function connexion() {
+    const buttonSubmit = document.querySelector("#submit")
 
-    let user = {
-        "email": "sophie.bluel@test.tld",
-        "password": "S0phie"
-    };
+    buttonSubmit.addEventListener("submit", function () {
 
+        const email = document.querySelector("#email").value
+        const password = document.querySelector("#motdepasse").value
 
+        const userData = {
+            email: email,
+            password: password,
+        }
+
+     connexion(userData)
+    })
+}
+
+async function connexion(userData) {
+    
 
     let response = await fetch("http://localhost:5678/api/users/login", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json;charset=utf-8'
         },
-        body: JSON.stringify(user)
+        body: JSON.stringify(userData)
     })
         .then(response => response.json())
 
-    alert(response.token);
-
-
-
-}
-
-function getValue(){
-    const value
+    alert(response);
 }
 
 
-connexion()
-getValue ()
+
+getValues()
+
+
